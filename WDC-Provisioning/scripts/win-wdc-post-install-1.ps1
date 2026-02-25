ecb17db4ebf96e96aa2b75c8ce8871cd618fb0fd
juronja <101410098+juronja@users.noreply.github.com>
2026-02-25 10:31:39 +0000

    try with fixed agent version

--- WDC-Provisioning/scripts/win-wdc-post-install-1.ps1
+++ WDC-Provisioning/scripts/win-wdc-post-install-1.ps1
@@ -72,7 +72,13 @@ if ($confirmation -match "^(y|yes)$") {
     Write-Host "Installing Wazuh..." -ForegroundColor Cyan
 
     $wazuhFQDN = Read-Host "Enter the Wazuh FQDN (eg. wazuh.lan)"
-    winget install -e --id Wazuh.WazuhAgent -s winget --override "/q WAZUH_MANAGER=$wazuhFQDN WAZUH_AGENT_GROUP=default WAZUH_AGENT_NAME=$newName"
+    $wazuhMngrVersion = Read-Host "Enter your current Wazuh Manager version to match the right agent version. (eg. 4.14.3)"
+    $majorVersion = $wazuhMngrVersion.Split('.')[0]
+
+    # winget install -e --id Wazuh.WazuhAgent -s winget --override "/q WAZUH_MANAGER=$wazuhFQDN WAZUH_AGENT_GROUP=default WAZUH_AGENT_NAME=$newName"
+
+    Invoke-WebRequest https://packages.wazuh.com/$majorVersion.x/windows/wazuh-agent-$wazuhMngrVersion-1.msi -OutFile $env:tmp\wazuh-agent
+    msiexec.exe /i $env:tmp\wazuh-agent /q WAZUH_MANAGER=$wazuhFQDN WAZUH_AGENT_GROUP='default' WAZUH_AGENT_NAME=$newName
 
     # Enable IIS logs
     $configPath = "C:\Program Files (x86)\ossec-agent\ossec.conf"
